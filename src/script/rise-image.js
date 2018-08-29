9da531d0a7a44e12a5cd24685aaaf01a9fe37a26
santiagonoguez <santiago@nul-unu.com>
2018-08-29 15:45:12 -0500

    add CSS styling demo

--- src/script/rise-image.js
+++ src/script/rise-image.js
@@ -26,8 +26,15 @@ class RiseImage extends PolymerElement {
 
   static get template() {
     return html`
+      <style>
+        img {
+          height: var(--image-height, 50px);
+          width: var(--image-width, 50px);
+          @apply --rise-image;
+        }
+      </style>
       <template is="dom-if" if="[[url]]">
-        <img src=[[url]] width="50" height="50">
+        <img src=[[url]]>
       </template>
       <template is="dom-if" if="[[!url]]">
         <p>Downloading image, please wait...</p>
