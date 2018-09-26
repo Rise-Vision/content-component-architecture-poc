bf4a8c0518789dbc00c8d4d3cdb8548acf0817bf
santiagonoguez <santiago@nul-unu.com>
2018-09-26 13:19:16 -0500

    remove enabler external scripts

--- src/script/rise-enabler-1.js
+++
@@ -1,5 +0,0 @@
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-
-// a simple enabler, with no functionality
-typeof PolymerElement;
-typeof html;
--- src/script/rise-enabler-2.js
+++
@@ -1,4 +0,0 @@
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-
-// a simple enabler, with no functionality
-typeof PolymerElement;
