5fd4d37b527e71de2e9128be56bb8f11c5bcac43
santiagonoguez <santiago@nul-unu.com>
2019-10-02 15:52:24 -0500

    dont publish on master

--- deploy_template.sh
+++ deploy_template.sh
@@ -11,7 +11,14 @@ do
 
   echo Deploying $STAGE version of $TEMPLATE to $TARGET
 
-  echo gsutil -m rsync -d -r $TEMPLATE/build/prod $TARGET
-  echo gsutil -m setmeta -r -h "Cache-Control:private, max-age=0" $TARGET
-  echo gsutil -m acl -r ch -u AllUsers:R $TARGET
+  if [[ "$CIRCLE_BRANCH" == "master" ]] # still not deploy on master
+  then
+    echo gsutil -m rsync -d -r $TEMPLATE/build/prod $TARGET
+    echo gsutil -m setmeta -r -h "Cache-Control:private, max-age=0" $TARGET
+    echo gsutil -m acl -r ch -u AllUsers:R $TARGET
+  else
+    gsutil -m rsync -d -r $TEMPLATE/build/prod $TARGET
+    gsutil -m setmeta -r -h "Cache-Control:private, max-age=0" $TARGET
+    gsutil -m acl -r ch -u AllUsers:R $TARGET
+  fi
 done
--- skip-if-no-master-template-changes.sh
+++ skip-if-no-master-template-changes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ "$CIRCLE_BRANCH" = "master" ]] && [[ ! -f build_tmp/template ]]
+if [[ "$CIRCLE_BRANCH" == "master" ]] && [[ ! -f build_tmp/template ]]
 then
     circleci-agent step halt
 fi
