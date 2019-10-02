df191d406835da383f3e21bd2fc3f04d7670f6b5
santiagonoguez <santiago@nul-unu.com>
2019-10-02 15:38:05 -0500

    simplify variable handling

--- deploy_template.sh
+++ deploy_template.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
 TEMPLATES_BASE=$1
-STAGE=$2
+STAGE_LIST=$2
 PRODUCT_CODE=$3
 TEMPLATE=$4
 
-STAGE_LIST=$STAGE
-if [[ $STAGE == 'all' ]]
-then
-  STAGE_LIST='staging,beta,stable'
-fi
-
-for current_stage in $( echo $STAGE_LIST | tr ',' '\n' )
+for STAGE in $( echo $STAGE_LIST | tr ',' '\n' )
 do
   TARGET=$TEMPLATES_BASE/$STAGE/templates/$PRODUCT_CODE
 
-  echo Deploying $STAGE_LIST version of $TEMPLATE to $TARGET
+  echo Deploying $STAGE version of $TEMPLATE to $TARGET
 
   echo gsutil -m rsync -d -r $TEMPLATE/build/prod $TARGET
   echo gsutil -m setmeta -r -h "Cache-Control:private, max-age=0" $TARGET
